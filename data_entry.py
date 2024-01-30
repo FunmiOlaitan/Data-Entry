2e1eb22ebcce21b2f336fed7c200aa1453a2161a
Funmilola Olaitan <funmilolaolaitan@yahoo.com>
2024-01-30 18:09:33 +0000

    Implement main program logic

--- data_entry.py
+++ data_entry.py
@@ -11,10 +11,10 @@ def get_membership_details():
 def display_details(details):
     print("\nMembership Details")
     for key, value in details.items():
-        print("f'{key}:{value}")
+        print(f'{key}: {value}')
 
 def save_membership(memberships):
-    with open("membership_details", "w") as file:
+    with open("membership_details.json", "w") as file:
         json.dump(memberships, file)
 
 def load_from_file():
@@ -30,3 +30,34 @@ def search_member(memberships, query):
             return member
     return None
 
+def main():
+    memberships = load_from_file()
+
+    while True:
+        new_member_details = get_membership_details()
+        display_details(new_member_details)
+
+        confirmation = input("Confirm membership details (Yes/No): ").lower()
+
+        if confirmation == "yes":
+            memberships.append(new_member_details)
+            save_membership(memberships)
+            print('Mmembership details saved')
+        else:
+            print("Membership details discarded ")
+        another_entry = ("Would you like to enter another membership? (Yes/No):").lower()
+        
+        if another_entry != "yes":
+            break
+    
+    search_query = input("Enter a name or email to search for a member: ")
+    found_member = search_member(memberships, search_query)
+
+    if found_member:
+        print("\nMember found: ")
+        display_details(found_member)
+    else:
+        print("\nMember not found ")
+    
+if __name__ == "__main__":
+    main()
